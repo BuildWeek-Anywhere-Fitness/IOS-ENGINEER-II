dcf7bb9ac1ab4d812fbfffef300e8f994efec712
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-23 16:30:00 -0700

    Added CRUD

--- AnywhereFitnessUnit2Build/AnywhereFitnessUnit2Build/Models/Class+Convenience.swift
+++ AnywhereFitnessUnit2Build/AnywhereFitnessUnit2Build/Models/Class+Convenience.swift
@@ -7,4 +7,45 @@
 //
 
 import Foundation
+import CoreData
 
+enum Category: String, CaseIterable {
+	case yoga
+	case HIIT
+	case powerLifting
+	case crossFit
+	case barre
+	case cycling
+	case dance
+}
+
+enum Duration: String, CaseIterable {
+	case halfHour = "30 minutes"
+	case hour = "60 minutes"
+	case hourAndHalf = "90 minutes"
+}
+
+enum Intensity: String, CaseIterable {
+	case beginner
+	case intermediate
+	case advanced
+}
+
+extension Class {
+	
+	@discardableResult convenience init?(name: String, category: Category, date: Date, duration: Duration, intensityLevel: Intensity, location: String, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+		
+		self.init(context: context)
+		
+		self.name = name
+		self.category = category.rawValue
+		self.date = date
+		self.duration = duration.rawValue
+		self.intensityLevel = intensityLevel.rawValue
+		self.location = location
+		
+	}
+	
+	
+	
+}
