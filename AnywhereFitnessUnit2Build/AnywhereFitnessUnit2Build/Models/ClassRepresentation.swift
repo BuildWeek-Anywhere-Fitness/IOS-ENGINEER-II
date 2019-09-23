13187f6c19ff8faddbb814c2c34a92cbc20454b5
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-09-23 11:52:02 -0800

    Add Project + Convenience + Storyboard

---
+++ AnywhereFitnessUnit2Build/AnywhereFitnessUnit2Build/Models/ClassRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  ClassRepresentation.swift
+//  AnywhereFitnessUnit2Build
+//
+//  Created by Alex Rhodes on 9/23/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import Foundation
---
+++ AnywhereFitnessUnit2Build/AnywhereFitnessUnit2Build/Models/TrainerRepresentatation.swift
@@ -0,0 +1,9 @@
+//
+//  TrainerRepresentatation.swift
+//  AnywhereFitnessUnit2Build
+//
+//  Created by Alex Rhodes on 9/23/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import Foundation
